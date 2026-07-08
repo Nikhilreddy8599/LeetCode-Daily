2733e34116d04ef276d1a2bf7982de2d57cab244
Nikhil <buzunoornikhil123@gmail.com>
2026-07-08 21:05:15 +0530

    Score of a String

---
+++ 3110.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int scoreOfString(String s) {
+        int x=s.length();
+        int sum=0;
+        for(int i=0;i<=x-2;i++){
+            int a=s.charAt(i);
+            int b=s.charAt(i+1);
+            sum=sum+Math.abs(a-b);
+        }
+        return sum;
+    }
+}
